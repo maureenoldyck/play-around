a8cd3a711dff10612ee262ccb67711d3cc17de91
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-01-15 09:37:16 +0100

    Add base files

---
+++ guessing Game/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// Require the correct variable type to be used (no auto-converting)
+declare(strict_types = 1);
+
+// Show errors so we get helpful information
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// Load you classes
+require_once 'classes/GuessingGame.php';
+
+// Start the game
+// As this game is rather simple, one class should be sufficient
+$game = new GuessingGame(3);
+$game->run();
+
+require 'view.html';
